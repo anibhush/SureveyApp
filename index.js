673f212463d83a114d77cbf5aedb54724f523cd7
anibhush <anisha.bhushan@gmail.com>
2020-07-08 12:50:36 +0530

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express(); //generates a new express application
+
+//create route handler
+app.get("/", (req, res) => {
+  res.send({ hi: "there" });
+});
+
+app.listen(5000);
